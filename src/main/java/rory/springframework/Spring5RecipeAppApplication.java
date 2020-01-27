e608627de36237a33be4044e31ec5947b253ad84
roryde <roryscanlon@gmail.com>
2020-01-27 06:13:49 +0000

    notes and recipe initial setup on domain

--- src/main/java/rory/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/rory/springframework/Spring5RecipeAppApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework;
+package rory.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
